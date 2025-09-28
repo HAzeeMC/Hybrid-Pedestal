da7c6016c62659453ab1424927494faba1ec9a81
HAzeeMC <nt5974649@gmail.com>
2025-09-28 08:18:02 +0700

    Create RecipeManager.java

---
+++ src/main/java/me/hybric/hybridpedestal/recipe/RecipeManager.java
@@ -0,0 +1,16 @@
+package me.hybric.hybridpedestal.recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeManager {
+    private final List<Recipe> recipes = new ArrayList<>();
+
+    public void addRecipe(Recipe recipe) {
+        recipes.add(recipe);
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+}
